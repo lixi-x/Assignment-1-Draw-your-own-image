8917f1a7be151bc6579806d49688783cf0165bf6
lixi-x <lixi-xixi@outlook.com>
2024-11-09 22:44:28 +0800

    drawing.py
    update

--- drawing.py
+++ drawing.py
@@ -1,51 +1,46 @@
-import turtle
+from turtle import *
 
-#set screen and pen
-screen = turtle.Screen()
-screen.title("smile face")
-screen.bgcolor("yellow")
-pen = turtle.Turtle()
-pen.speed(10)
-pen.pensize(10)
+
+title("smile face")
+bgcolor("yellow")
+speed(3)
+pensize(10)
 
 #the outline of smile face
-pen.penup()
-pen.goto(0,-200)
-pen.pendown()
-pen.circle(200)
+penup()
+goto(0,-200)
+pendown()
+circle(200)
 
 #left eye
-pen.color("black")
-pen.penup()
-pen.goto(-50,50)
-pen.pendown()
-pen.setheading(90)
-pen.circle(50,180)
+color("black")
+penup()
+goto(-50,50)
+pendown()
+seth(90)
+circle(50,180)
 
 #right eye
-pen.color("black")
-pen.penup()
-pen.goto (120,100)
-pen.pendown()
-pen.goto (50,65)
-pen.goto (120,30)
+color("black")
+penup()
+goto (120,100)
+pendown()
+goto (50,65)
+goto (120,30)
 
 #mouth
-pen.color("red")
-pen.penup()
-pen.goto(75,-75)
-pen.pendown()
-pen.setheading(90)
-pen.circle(75,-180)
+color("red")
+penup()
+goto(75,-75)
+pendown()
+seth(90)
+circle(75,-180)
 
 #write
-pen.color("blue")
-pen.penup()
-pen.goto(-180,250)
-pen.pendown
-pen.write("happy everyday",font=("Courier", 30, "bold"))
+color("blue")
+penup()
+goto(-180,250)
+pendown
+write("happy everyday",font=("Courier", 30, "bold"))
 
-pen.hideturtle()
-screen.exitonclick
-
-
+done()
